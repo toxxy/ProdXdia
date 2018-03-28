e876b59b99e43dc7aa90b47a2e8c75b8d9b79711
Emmanuel Cardenaz <cardenaz_1000@hotmail.com>
2018-03-28 11:07:17 -0500

    Finished

--- ProdXdia_Conv/ProdXdia_Conv/ProdXdia_Conv/Form1.cs
+++ ProdXdia_Conv/ProdXdia_Conv/ProdXdia_Conv/Form1.cs
@@ -10,10 +10,27 @@ using System.Windows.Forms;
 namespace ProdXdia_Conv
 {
     public partial class Form1 : Form
-    {
+    { 
+        EAC_Framework.eac_dataGridView  gridView;
+        EAC_Framework.eac_sqlConnector connection; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //HELLO 
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new EAC_Framework.eac_sqlConnector("rio-sql01", "prodxdia", "", "cgaadmin", "cgaadmin201");
+            gridView = new EAC_Framework.eac_dataGridView(ref dataGridView1, ref connection);
+            gridView.fillGridFromSqlSelect("select * FROM [prodxdia].[dbo].[prod_diaria]", ref connection);
+
         }
     }
 }
